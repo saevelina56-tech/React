1cf7b89d58ed9b75e34d1369f9c0349630b93cd2
saevelina56-tech <saevelina56@gmail.com>
2026-05-20 19:31:39 +0800

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import EffectComponent from './ResizableBox'
-import DataFetcherContainer from './DataFetcher'
+import React, { useState } from 'react'
+import UserCard from './UserCard'
+import ButtonWithCallback from './ButtonWithCallback'
 
 function App() {
+  const [name] = useState('John')
+  const [age, setAge] = useState(25)
+  const [color, setColor] = useState('blue');
+  function changeAge() {
+    setAge(age+1);
+  }
+
+  function changeColor() {
+    const colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+  }
+
+  function handleClick(message) {
+    console.log(message);
+  }
+
   return (
     <>
-      <DataFetcherContainer />
-      <EffectComponent />
+      <h1>Первое задание</h1>
+      <button onClick={changeAge}>Увеличить возраст на 1 год</button>
+      <button onClick={changeColor}>Изменить цвет</button>
+      <UserCard name={name} age={age} color={color} />
+      <h1>Второе задание</h1>
+      <ButtonWithCallback onButtonClick={handleClick} />
+
     </>
   )
 }
 
-export default App
+export default App;
