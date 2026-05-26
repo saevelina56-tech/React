b9246a369f4800af1486b2b3e7dfd8457042ad01
saevelina56-tech <saevelina56@gmail.com>
2026-05-26 21:42:15 +0800

    Первое задание: UserStatus

---
+++ src/UserStatus.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react'
+
+function UserStatus ({isLoggedIn}) {
+    useEffect(() => {
+        if (isLoggedIn) {
+            console.log('Пользователь вошел в систему');
+        } else {
+            console.log('Пользователь вышел из системы');
+        }
+    }, [isLoggedIn]);
+    if (isLoggedIn) {
+        return (
+            <div>
+                <h2>Добро пожаловать, пользователь!</h2>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <button>
+                    Войти
+                </button>
+            </div>
+        );
+    }
+}
+
+export default UserStatus;
+
