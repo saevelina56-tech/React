135e58a4a84b3ca17a77995ee0dc4dd782b40277
saevelina56-tech <saevelina56@gmail.com>
2026-05-28 21:30:34 +0800

    второе задание: Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,77 @@
-import React, { useState } from 'react'
-import UserStatus from './UserStatus'
-import FruitList from './FruitList';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import UserCard from './UserCard';
 
+function Home() {
+    return (
+      <h1>Это главная страница</h1>
+    )
+  }
+  function AboutUs() {
+    return (
+      <div>
+        <h1>О нас: </h1>
+        <p>аывораылоравоалыовалво аооаао аоылдффд</p>
+      </div>
+    )
+  }
 
+  function Contacts() {
+    return (
+      <div>
+        <p>+7 812 7227 91</p>
+        <p>+7 812 7347 55</p>
+      </div>
+    )
+  }
+  function Navigation() {
+    return (
+      <nav>
+        <Link to = '/'
+          onMouseEnter={(e) => e.target.style.backgroundColor = '#34495e'}
+          onMouseLeave={(e) => e.target.style.backgroundColor = 'transparent'}
+          >
+          Главная
+        </Link>
+        <Link to = '/about'>
+          О нас
+        </Link>
+        <Link to ='/contacts'>
+          Контакты
+        </Link>
+      </nav>
+    );
+  }
 function App() {
+  const users = [
+        { name: 'Анна Иванова', age: 25, email: 'anna@example.com' },
+        { name: 'Пётр Сидоров', age: 32, email: 'petr@example.com' },
+        { name: 'Мария Петрова', age: 28, email: 'maria@example.com' },
+    ];
+  
 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  const toggleLoginStatus = () => {
-    setIsLoggedIn(prevStatus => !prevStatus);
-  };
-
-  const fruits = ['Яблоко', 'Банан', 'Апельсин', 'Киви', 'Виноград']
   return (
     <>
-       <UserStatus isLoggedIn={isLoggedIn} />
-       <div>
-          <button
-            onClick={toggleLoginStatus}
-          >
-            {isLoggedIn ? 'Выйти' : 'Войти'}
-          </button>
-       </div>
-       <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-        <h2>Домашнее задание: Рендеринг списков</h2>
-        <FruitList fruits={fruits} />
-       </div>
+      <h1>Первое задание</h1>
+       <h2>Карточки пользователей</h2>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', marginTop: '20px' }}>
+          {users.map(user => (
+              <UserCard 
+                  name={user.name}
+                  age={user.age}
+                  email={user.email}
+              />
+          ))}
+        </div>
+        <h1>Второе задание</h1>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<AboutUs />}/>
+            <Route path='/contacts' element={<Contacts />}/>
+          </Routes>
+        </BrowserRouter>
     </>
   )
 }
